f170f5536af3a8ecb343221c2ba7113df0e67b2d
Mike <Mike@auth-10-3-1-222.wifi.mphnetinc.net>
2016-02-13 19:56:10 -0700

    Incremental Commit

--- lcd.c
+++ lcd.c
@@ -1,15 +1,7 @@
-/*
- * File:   lcd.c
- * Authors:
- *
- * Created on December 31, 2014, 1:39 PM
- */
-
-/* TODO: Make define statements for each pin used in the LCD
- */
 #include <xc.h>
 #include "lcd.h"
 #include "timer.h"
+#include <string.h>
 
 #define TRISx_INPUT   1
 #define TRISx_OUTPUT  0
@@ -21,37 +13,55 @@
 #define LCD_D6  LATEbits.LATE3
 #define LCD_D7  LATEbits.LATE1
 
-/* This function should take in a two-byte word and writes either the lower or upper
- * byte to the last four bits of LATE. Additionally, according to the LCD data sheet
- * It should set LCD_RS and LCD_E with the appropriate values and delays.
- * After these commands are issued, there should be a delay.
- * The command type is a simplification. From the data sheet, the RS is '1'
- * when you are simply writing a character. Otherwise, RS is '0'.
- */
-void writeFourBits(unsigned char word, unsigned int commandType, unsigned int delayAfter, unsigned int lower){
-    //TODO:
-    // set the commandType (RS value)
-    LATEbits.LATE0 = word&0x01;
-    LATEbits.LATE2 = word&0x02;
-    LATEbits.LATE4 = word&0x04;
-    LATEbits.LATE6 = word&0x08;
-    
-    //enable
-    //delay
-    //disable
-}
-
-/* Using writeFourBits, this function should write the two bytes of a character
- * to the LCD.
- */
-void writeLCD(unsigned char word, unsigned int commandType, unsigned int delayAfter){
-    //TODO:
+void LCD_Write(unsigned char word, unsigned int commandType, unsigned int delayAfter){
+  /*  Set LCD_RS  */
+  if(commandType == 0)LCD_RS = 0;
+  else LCD_RS = 1;
+  
+  /*  Write First 4-bits of Word  */
+  LCD_D7 = word&0x80;LCD_D6 = word&0x40;LCD_D5 = word&0x20;LCD_D4 = word&0x10;
+  //Enable
+  LCD_E = 1;
+  //Delay
+  delayUs(delayAfter);
+  //Disable
+  LCD_E = 0;
+  
+  /*  Write Last 4-bits of Word  */
+  LCD_D7 = word&0x08;LCD_D6 = word&0x04;LCD_D5 = word&0x02;LCD_D4 = word&0x01;
+  //Enable
+  LCD_E = 1;
+  //Delay
+  delayUs(delayAfter);
+  //Disable
+  LCD_E = 0;  
 }
 
 /* Given a character, write it to the LCD. RS should be set to the appropriate value.
  */
-void printCharLCD(char c){
-    //TODO:
+void LCD_PrintChar(char c){
+  LCD_Write(c,1,50);
+}
+
+/*
+ * You can use printCharLCD here. Note that every time you write a character
+ * the cursor increments its position automatically.
+ * Since a string is just a character array, try to be clever with your use of pointers.
+ */
+void LCD_PrintString(const char* s) {
+  int x = 0;
+  for(x = 0;x<strlen(s);x++)LCD_PrintChar(s[x]);
+}
+
+void clearLCD(){
+  
+}
+
+/*
+ Use the command for changing the DD RAM address to put the cursor somewhere.
+ */
+void LCD_MoveCursor(unsigned char x, unsigned char y){
+  
 }
 
 void initLCD(void){//Done
@@ -142,27 +152,6 @@ void initLCD(void){//Done
 }
 
 /*
- * You can use printCharLCD here. Note that every time you write a character
- * the cursor increments its position automatically.
- * Since a string is just a character array, try to be clever with your use of pointers.
- */
-void printStringLCD(const char* s) {
-    //TODO:
-}
-
-/*
- * Clear the display.
- */
-void clearLCD(){
-}
-
-/*
- Use the command for changing the DD RAM address to put the cursor somewhere.
- */
-void moveCursorLCD(unsigned char x, unsigned char y){
-}
-
-/*
  * This function is called in lab1p2.c for testing purposes.
  * If everything is working properly, you should get this to look like the video on D2L
  * However, it is suggested that you test more than just this one function.
@@ -170,12 +159,12 @@ void moveCursorLCD(unsigned char x, unsigned char y){
 void testLCD(){
     initLCD();
     int i = 0;
-    printCharLCD('c');
+    LCD_PrintChar('c');
     for(i = 0; i < 1000; i++) delayUs(1000);
     clearLCD();
-    printStringLCD("Hello!");
-    moveCursorLCD(1, 2);
+    LCD_PrintString("Hello!");
+    LCD_MoveCursor(1, 2);
     for(i = 0; i < 1000; i++) delayUs(1000);
-    printStringLCD("Hello!");
+    LCD_PrintString("Hello!");
     for(i = 0; i < 1000; i++) delayUs(1000);
 }
