59d9b15c62dcdfc682c79d0d950546610b1378f6
Mike <Mike@dhcp-10-142-174-41.uawifi.arizona.edu>
2016-02-18 16:42:15 -0700

    Partially Working LCD_OtherTest

--- lcd.c
+++ lcd.c
@@ -8,10 +8,10 @@
 
 #define LCD_RS  LATCbits.LATC4
 #define LCD_E   LATCbits.LATC2
-#define LCD_D4  LATEbits.LATE7
-#define LCD_D5  LATEbits.LATE5
-#define LCD_D6  LATEbits.LATE3
-#define LCD_D7  LATEbits.LATE1
+#define LCD_D4  LATEbits.LATE1
+#define LCD_D5  LATEbits.LATE3
+#define LCD_D6  LATEbits.LATE5
+#define LCD_D7  LATEbits.LATE7
 
 void LCD_Write(unsigned char word, unsigned int commandType, unsigned int delayAfter){
   /*  Set LCD_RS  */
@@ -70,6 +70,7 @@ void LCD_Init(void){
   TRISDbits.TRISD0 = TRISx_OUTPUT;
   LATDbits.LATD0 = 0;
   // Set Tristate Registers
+  ANSELEbits.ANSE7 = 0;
   TRISCbits.TRISC4 = TRISx_OUTPUT;// LCD_RS
   TRISCbits.TRISC2 = TRISx_OUTPUT;// LCD_E
   TRISEbits.TRISE7 = TRISx_OUTPUT;// LCD_D7
@@ -84,135 +85,127 @@ void LCD_Init(void){
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 1;
   LCD_E = 1;
-  delayUs(5000);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(5000);
   
   //Second Initialization Sequence
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 1;
   LCD_E = 1;
-  delayUs(200);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(150);
   
   //Third Initialization Sequence
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 1;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   
   //Set 4-bit Interface
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   
   //Function Set
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   LCD_RS = 0;
-  LCD_D7 = 1;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
+  LCD_D7 = 1;LCD_D6 = 1;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(1);
   
   //Turn Display Off
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   LCD_RS = 0;
   LCD_D7 = 1;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
   
   //Clear Display
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 1;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
   
   //Entry Mode Set
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 1;LCD_D5 = 1;LCD_D4 = 1;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
   
   //Turn Display On, Cursor Off Blink Off
   LCD_RS = 0;
   LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
+  delayUs(10);
   LCD_RS = 0;
   LCD_D7 = 1;LCD_D6 = 1;LCD_D5 = 0;LCD_D4 = 0;
   LCD_E = 1;
-  delayUs(100);
+  delayUs(10);
   LCD_E = 0;
   LATDbits.LATD0 = 1;
 }
 
-/*
- * This function is called in lab1p2.c for testing purposes.
- * If everything is working properly, you should get this to look like the video on D2L
- * However, it is suggested that you test more than just this one function.
- */
 void LCD_OtherTest(){
   LCD_RS = 1;
   /*  Write First 4-bits of Word  */
-  LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 1;LCD_D4 = 1;
+  LCD_D7 = 0;LCD_D6 = 1;LCD_D5 = 0;LCD_D4 = 0;
   //Enable
   LCD_E = 1;
   //Delay
-  delayUs(50);
+  delayUs(10);
   //Disable
   LCD_E = 0;
+  delayUs(10);
   /*  Write Last 4-bits of Word  */
-  LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 0;
+  LCD_RS = 1;
+  LCD_D7 = 0;LCD_D6 = 0;LCD_D5 = 0;LCD_D4 = 1;
   //Enable
   LCD_E = 1;
   //Delay
-  delayUs(50);
+  delayUs(10);
   //Disable
   LCD_E = 0;
+  
   int x = 0;
-  for(x = 0; x < 1000; x++) delayUs(1000);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
-  //LCD_PrintChar(0b11111111);
+  for(x = 0; x < 100; x++) delayUs(100);
 }
-void testLCD(){
+void LCD_Test(){
     int i = 0;
     LCD_PrintChar('c');
     for(i = 0; i < 1000; i++) delayUs(1000);
